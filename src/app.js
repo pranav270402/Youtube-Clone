6efd5a950952e3f59a9b6adbbe9f13bbc78b31e7
pranav <pmpranav2002@gmail.com>
2024-12-17 19:56:53 +0530

    controller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//Import Routes
+import userRoutes from "./routes/user.routes.js";
+
+//Route Declaration
+app.use("/api/v1/user", userRoutes);
+
 export default app
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+
+const userRegister= asyncHandler(async (req, res)=>{
+    res.status(200).json({message: "Ok"})
+})
+
+export {userRegister}
